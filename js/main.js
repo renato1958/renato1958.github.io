b4c7ae3619b9e759365a5bd4007c7101e2c95601
renato1958 <felisi.renato@gmail.com>
2020-09-11 14:42:59 +0200

    Conferita funzionalità allo slider green

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const alphaSlider = document.querySelector('.alphaslider');
-const redSlider = document.querySelector('.redslider');
 const alphaValueDisp = document.querySelector('.alphavalue');
+const redSlider = document.querySelector('.redslider');
 const redValueDisp = document.querySelector('.redvalue');
+const greenSlider = document.querySelector('.greenslider');
+const greenValueDisp = document.querySelector('.greenvalue');
 
 const overlay = document.querySelector('.overlay');
 
@@ -11,8 +13,13 @@ let blueValue = 0;
 let alphaValue = 1;
 
 overlay.style.backgroundColor = `rgba(${redValue}, ${greenValue}, ${blueValue}, ${alphaValue})`;
+
 alphaSlider.value = `${alphaValue}`;
+alphaValueDisp.innerHTML = `${alphaValue}`;
 redSlider.value = `${redValue}`;
+redValueDisp.innerHTML = `${redValue}`;
+greenSlider.value = `${greenValue}`;
+greenValueDisp.innerHTML = `${greenValue}`;
 
 alphaSlider.addEventListener('input', () => {
 	alphaValue = alphaSlider.value;
@@ -23,6 +30,12 @@ alphaSlider.addEventListener('input', () => {
 redSlider.addEventListener('input', () => {
         redValue = redSlider.value;
         redValueDisp.innerHTML = `${redValue}`;
+        overlay.style.backgroundColor = `rgba(${redValue}, ${greenValue}, ${blueValue}, ${alphaValue})`;
+}, false);
+
+greenSlider.addEventListener('input', () => {
+        greenValue = greenSlider.value;
+        greenValueDisp.innerHTML = `${greenValue}`;
         overlay.style.backgroundColor = `rgba(${redValue}, ${greenValue}, ${blueValue}, ${alphaValue})`;
 }, false);
 
