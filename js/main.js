664c7de868f7b89c52018172a61e2a435526c514
renato1958 <felisi.renato@gmail.com>
2020-09-11 14:24:58 +0200

    Conferita funzionalità allo slider red

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const alphaSlider = document.querySelector('.alphaslider');
+const redSlider = document.querySelector('.redslider');
 const alphaValueDisp = document.querySelector('.alphavalue');
+const redValueDisp = document.querySelector('.redvalue');
+
 const overlay = document.querySelector('.overlay');
 
 let redValue = 0;
@@ -9,9 +12,18 @@ let alphaValue = 1;
 
 overlay.style.backgroundColor = `rgba(${redValue}, ${greenValue}, ${blueValue}, ${alphaValue})`;
 alphaSlider.value = `${alphaValue}`;
+redSlider.value = `${redValue}`;
 
 alphaSlider.addEventListener('input', () => {
 	alphaValue = alphaSlider.value;
 	alphaValueDisp.innerHTML = `${alphaValue}`;
 	overlay.style.backgroundColor = `rgba(${redValue}, ${greenValue}, ${blueValue}, ${alphaValue})`;
 }, false);
+
+redSlider.addEventListener('input', () => {
+        redValue = redSlider.value;
+        redValueDisp.innerHTML = `${redValue}`;
+        overlay.style.backgroundColor = `rgba(${redValue}, ${greenValue}, ${blueValue}, ${alphaValue})`;
+}, false);
+
+
